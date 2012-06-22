8167d3bb578705c904a685ecb36caa636bbb077e
i6rz <i6rz13@gmial.com>
2012-06-22 13:22:45 +0800

    first commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have cod like this." # and the comment after is igored
+
+# you can also use a comment to "disable" or comment out a pice of code:
+# print "This won't run."
+
+print "This will run."
