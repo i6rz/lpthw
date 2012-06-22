7c114facb2531bb459fd21c2b1fe7e7d7c0765a5
i6rz <i6rz13@gmial.com>
2012-06-22 16:39:38 +0800

    update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,6 +1,7 @@
 ﻿# Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# '\n' means switch to another line.
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days:", days
