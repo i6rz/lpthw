147b7098dd99bdca34990da17e2910162e58871c
i6rz <i6rz13@gmial.com>
2012-06-22 16:11:55 +0800

    add ex8.py

---
+++ ex8.py
@@ -0,0 +1,13 @@
+﻿formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+	"That you could type up right.",
+	# Why the following line use double quotes?
+	"But it didn't sing.",
+	"So I said goodnight."
+	)
